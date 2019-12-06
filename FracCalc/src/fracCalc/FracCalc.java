9a0be43902e2ffdb05ad02281ffb78a6b6ead311
postunat000 <postunat000@CAM-LAB-22.bgsd.k12.wa.us>
2019-12-06 07:52:10 -0800

    BetterThanLastTime.java

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -11,36 +11,21 @@ public class FracCalc {
 
     public static void main(String[] args) {
     	Scanner console = new Scanner(System.in);
-    	
-        // TODO: Read the input from the user and call produceAnswer with an equation
-    	
     	String input;
-    	System.out.print("What do you want bro ");
-    	input = console.next();
-    	
-    	String second = produceAnswer(input);
-    	System.out.println("The second fraction is " + second);
+    	while (input != "quit")
+        System.out.print("What calculation would you like me to perform for you, my liege? ");
+        input = console.nextLine();
+
+        String second = produceAnswer(input);
+        System.out.println("The second fraction is " + second);
+        System.out.println();
+        }
     }
     
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input) { 
-        // TODO: Implement this function to produce the solution to the input
-
-    	String inputOne = input + " ";
-    	int length = inputOne.length();
+    	int length = input.length();
+    	int space = input.indexOf(" ") + 3;
     	
-    	int space = input.indexOf(" ");
-    	int first = space + 5;
-    	return inputOne.substring(first, length);
+    	return input.substring(space, length);
     }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-    
 }
